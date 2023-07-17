492db0f98e66a8e7e400238cb7caf70b0ecca25a
nirazul <rouven@adorable-squid.ch>
2023-07-17 13:59:06 +0200

    Add command for gaze long

--- src/models/DialogCommand/DialogCommandAddHotspot.ts
+++ src/models/DialogCommand/DialogCommandAddHotspot.ts
@@ -4,7 +4,9 @@ export interface DialogCommandSpecAddHotspot extends arg.Spec {
   '--x': arg.Handler<number>
   '--y': arg.Handler<number>
   '--z': arg.Handler<number>
+  '--gaze-duration': arg.Handler<number>
   '--click': [arg.Handler<string>]
+  '--gaze-long': [arg.Handler<string>]
 }
 
 export type DialogCommandResultAddHotspot = arg.Result<DialogCommandSpecAddHotspot> & {
@@ -12,13 +14,17 @@ export type DialogCommandResultAddHotspot = arg.Result<DialogCommandSpecAddHotsp
   y: number
   z?: number
   click: Array<string>
+  gazeDuration?: number
+  gazeLong?: Array<string>
 }
 
 export const dialogCommandSpecAddHotspot: arg.Spec = {
   '--x': Number,
   '--y': Number,
   '--z': Number,
+  '--gaze-duration': Number,
   '--click': [String],
+  '--gaze-long': [String],
   '-x': '--x',
   '-y': '--y',
   '-z': '--z',
